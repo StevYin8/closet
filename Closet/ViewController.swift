5bb487a6d08552bf7e19c48117e2b1acba9bdab6
StevYin8 <stev.yin.425@gmail.com>
2025-03-10 12:22:36 +0800

    Initial Commit

---
+++ Closet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Closet
+//
+//  Created by 尹少军 on 2025/3/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ClosetTests/ClosetTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClosetTests.swift
+//  ClosetTests
+//
+//  Created by 尹少军 on 2025/3/10.
+//
+
+import Testing
+@testable import Closet
+
+struct ClosetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
